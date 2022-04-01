f67919f3018410853f1b846e670c40bfa3e7a3a2
Róbert <robert.suchy5@gmail.com>
2022-04-01 14:22:12 +0200

    Algorand Standard Asset added

--- eco_label_app/smart_contract.py
+++ eco_label_app/smart_contract.py
@@ -1,22 +1,26 @@
+from re import L
 from pyteal import *
 from pyteal.ast.bytes import Bytes
 from pyteal_helpers import program
 
 
 def approval():
-    local_customer = Bytes("customer") # byteslice
-    local_reward = Bytes("reward") # uint64
+    global_escrow = Bytes("escrow") # byteslice
+    # local_customer = Bytes("customer") # byteslice
+    # local_reward = Bytes("reward") # uint64
     
     # op_add = Bytes("add")
     op_send_reward = Bytes("send_reward")
+    op_set_escrow = Bytes("set_escrow")
+    op_create_asa = Bytes("create_asa")
     # op_c2c = Bytes("c2c")
 
-    @Subroutine(TealType.none)
-    def init(account: Expr):
-        return Seq(
-            App.localPut(account, local_customer, Txn.application_args[0]),
-            App.localPut(account, local_reward, Btoi(Txn.application_args[1]))
-        )
+    # @Subroutine(TealType.none)
+    # def init(account: Expr):
+    #     return Seq(
+    #         App.localPut(account, local_customer, Txn.application_args[0]),
+    #         App.localPut(account, local_reward, Btoi(Txn.application_args[1]))
+    #     )
 
     # @Subroutine(TealType.none)
     # def add(account: Expr):
@@ -32,14 +36,36 @@ def approval():
             InnerTxnBuilder.SetFields(
                 {
                     TxnField.type_enum: TxnType.Payment,
-                    TxnField.receiver: App.localGet(account, local_customer),
-                    TxnField.amount: App.localGet(account, local_reward),
+                    TxnField.receiver: Txn.application_args[1],
+                    TxnField.amount: Btoi(Txn.application_args[2]),
                     TxnField.fee: Int(0)
                 }
             ),
             InnerTxnBuilder.Submit(),
             Approve()
         )
+
+    @Subroutine(TealType.none)
+    def set_escrow():
+        return Seq([
+            Assert(Txn.sender() == Global.creator_address()),
+            App.globalPut(global_escrow, Txn.application_args[1]),
+            Approve()
+        ])
+
+    @Subroutine(TealType.none)
+    def create_asa():
+        return Seq([
+            Assert(Global.group_size() == Int(2)),
+            Assert(Gtxn[1].sender() == App.globalGet(global_escrow)),
+            Assert(
+                And(
+                    Gtxn[1].type_enum() == TxnType.AssetConfig,
+                    Eq(Gtxn[1].config_asset_name(), Bytes("EcoLabel Coin")),
+                )
+            ),
+            Approve()
+        ])
 
     # @Subroutine(TealType.none)
     # def c2c():
@@ -62,13 +88,15 @@ def approval():
     return program.event(
         init = Approve(),
         opt_in = Seq( 
-            init(Int(0)),
+            # init(Int(0)),
             Approve()
         ),
         no_op = Seq(
             Cond(
                 # [Txn.application_args[0] == op_add, add(Int(0))],
                 [Txn.application_args[0] == op_send_reward, send_reward(Int(0))],
+                [Txn.application_args[0] == op_set_escrow, set_escrow()],
+                [Txn.application_args[0] == op_create_asa, create_asa()]
                 # [Txn.application_args[0] == op_c2c, c2c()],
             ),
             Reject()
--- eco_label_app/views.py
+++ eco_label_app/views.py
@@ -3,8 +3,50 @@ from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 from json import loads
 from .models import *
+from base64 import b64decode
+from typing import Tuple
+from algosdk.v2client.algod import AlgodClient
+from algosdk.future import transaction
+from algosdk import account
+from pyteal import compileTeal, Mode, Expr
+from .smart_contract import approval_program, clear_state_program
 
 # Create your views here.
+
+# APPROVAL_PROGRAM = b""
+# CLEAR_STATE_PROGRAM = b""
+
+# def compile_contract(client: AlgodClient, contract: Expr) -> bytes:
+#     teal = compileTeal(contract, mode=Mode.Application, version=6)
+#     response = client.compile(teal)
+#     return b64decode(response["result"])
+
+
+# def get_smart_contract(client: AlgodClient) -> Tuple[bytes, bytes]:
+#     global APPROVAL_PROGRAM
+#     global CLEAR_STATE_PROGRAM
+
+
+#     if len(APPROVAL_PROGRAM) == 0:
+#         APPROVAL_PROGRAM = compile_contract(client, approval_program())
+#         CLEAR_STATE_PROGRAM = compile_contract(client, clear_state_program())
+
+#     return APPROVAL_PROGRAM, CLEAR_STATE_PROGRAM
+
+
+# def createApp(client: AlgodClient, sender: account):
+#     approval_program, clear_state_program = get_smart_contract(client)
+#     global_schema = transaction.StateSchema(num_uints=0, num_byte_slices=0)
+#     local_schema = transaction.StateSchema(num_uints=0, num_byte_slices=0)
+#     app_args = []
+    
+#     txn = transaction.ApplicationCreateTxn(
+#         sender=sender
+#     )
+
+
+# def createToken():
+
 
 @csrf_exempt
 def process_products(request):
@@ -14,11 +56,15 @@ def process_products(request):
     for item in input_body['products']:
         reward += Products.objects.get(id=item['id']).rating * item['price'] * item['amount'] * 1000
     print(reward)
+    # appCallTxn = transaction.ApplicationCallTxn(
+    #     sender=
+    # )
     return HttpResponse(status=200)
     
 
 @csrf_exempt
 def add_product(request):
     input_body = loads(request.body)
-    
+
     return HttpResponse(status=200)
+    
